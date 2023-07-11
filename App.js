ba313cdf640f23922bb4865d74c2f65c6f7b0333
ksuchit <suchitkore1528@gmail.com>
2023-07-11 18:10:46 +0530

    drawer added but need to see

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { Image, Pressable, StatusBar, StyleSheet, View } from "react-native";
+import { Button, Image, Pressable, StatusBar, StyleSheet, View } from "react-native";
 import {
+  DrawerActions,
   NavigationContainer,
   useFocusEffect,
   useNavigation,
@@ -11,11 +12,49 @@ import TopTabs from "./components/TopTabs";
 import { useCallback, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ToastProvider } from "react-native-toast-notifications";
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { DrawerContentScrollView, DrawerItem, DrawerItemList, createDrawerNavigator } from "@react-navigation/drawer";
+import HomeTopTabs from "./components/HomeTopTabs";
+import Todo from "./components/Todo";
+import OnboardingScreens from "./components/OnboardingScreens";
 
 const Stack = createNativeStackNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+      <DrawerItem
+        label="Toggle drawer"
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+    </DrawerContentScrollView>
+  );
+}
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
 
 function MyStack() {
   const [userDetails, setUserDetails] = useState({});
@@ -32,14 +71,20 @@ function MyStack() {
   );
   return (
     <Stack.Navigator
-      initialRouteName="Login"
+      initialRouteName="OnBoarding"
       screenOptions={{
         headerStyle: {
           backgroundColor: "#f0f0f5",
         },
         headerShadowVisible: false,
         headerRight: () => (
-          <Pressable onPress={() => navigation.navigate("Profile")}>
+          // <MyDrawer />
+          <Pressable onPress={() => {
+            // navigation.navigate("Profile")
+            navigation.dispatch(DrawerActions.openDrawer())
+            navigation.navigate('Drawer')
+            console.log("Header profile clicked")
+          }}>
             {userDetails?.picture ? (
               <Image
                 style={{
@@ -70,6 +115,11 @@ function MyStack() {
       }}
     >
       <Stack.Screen
+        name="OnBoarding"
+        component={OnboardingScreens}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
         name="Auth"
         component={TopTabs}
         options={{ headerShown: false }}
@@ -81,11 +131,30 @@ function MyStack() {
           title: "Todo",
         }}
       />
+      <Stack.Screen
+        name="Drawer"
+        component={MyDrawer}
+        options={{headerShown:false}}
+      />
     </Stack.Navigator>
   );
 }
 
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+      <Drawer.Navigator screenOptions={{ drawerLabel: "Hello sk" ,drawerPosition:'right'}}
+        drawerContent={(props)=> <CustomDrawerContent {...props} />}
+      >
+         <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+  );
+}
+
 export default function App() {
+
   return (
     <View style={styles.container}>
       <ToastProvider
@@ -94,15 +163,20 @@ export default function App() {
         offset={100}
         animationDuration={250}
         animationType="zoom-in"
-        successIcon={<Ionicons name="md-checkmark-done-sharp" size={20} color={'white'} />}
-        warningIcon={<AntDesign name="warning" size={20} color={'white'} />}
-        dangerIcon={<MaterialIcons name="dangerous" size={20} color={'white'} />}
+        successIcon={
+          <Ionicons name="md-checkmark-done-sharp" size={20} color={"white"} />
+        }
+        warningIcon={<AntDesign name="warning" size={20} color={"white"} />}
+        dangerIcon={
+          <MaterialIcons name="dangerous" size={20} color={"white"} />
+        }
         normalColor="#2196F3"
         // icon={<AntDesign name="flag" size={20} color={'white'} />}
       >
         <NavigationContainer>
           {/* <StatusBar backgroundColor={"#fofof5"} barStyle="dark-content" /> */}
-          <MyStack />
+          {/* <MyDrawer /> */}
+            <MyStack />
         </NavigationContainer>
       </ToastProvider>
     </View>
